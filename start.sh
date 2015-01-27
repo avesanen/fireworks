9c7ef09cab9a6e8c5a49f8ada7ee664d692aaca7
Antti Vesanen <antti.vesanen@iki.fi>
2015-01-27 21:32:59 +0200

    rename

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t wsgui .
-docker stop wsgui
-docker rm wsgui
-docker run -v /var/lib/wsgui:/go/src/github.com/avesanen/wsgui/db -p 127.0.0.1:5002:8000 -d --name wsgui wsgui
+docker build -t fireworks .
+docker stop fireworks
+docker rm fireworks
+docker run -v /var/lib/fireworks:/go/src/github.com/avesanen/fireworks/db -p 127.0.0.1:5003:8000 -d --name fireworks fireworks
